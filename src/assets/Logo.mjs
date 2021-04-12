1cd6bc7f4d6051875fff57457505902e85163469
jaeh <jascha@jaeh.at>
2021-04-12 14:15:47 +0200

    src/assets/Logo: change logo css to overwrite theme

--- src/assets/Logo.mjs
+++ src/assets/Logo.mjs
@@ -1,1 +1,12 @@
 export const View = () => img({ class: 'Logo', alt: 'Artificial Museum', title: 'Artificial Museum', src: 'https://static.artificialmuseum.com/favicon.png' })
+
+export const style = vars => ({
+  '.Header &&': {
+    float: 'left',
+  },
+  [`@media screen and (min-width: ${vars.widths.laptop}`]: {
+    '.Header &&': {
+      float: 'left',
+    },
+  },
+})
