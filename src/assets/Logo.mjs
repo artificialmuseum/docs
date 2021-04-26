4e3a25e1916cd00ae469b189fa307260392dd9f8
jaeh <jascha@jaeh.at>
2021-04-26 10:31:34 +0200

    Logo: format

--- src/assets/Logo.mjs
+++ src/assets/Logo.mjs
@@ -1,4 +1,10 @@
-export const View = () => img({ class: 'Logo', alt: 'Artificial Museum', title: 'Artificial Museum', src: 'https://static.artificialmuseum.com/favicon.png' })
+export const View = () =>
+  img({
+    class: 'Logo',
+    alt: 'Artificial Museum',
+    title: 'Artificial Museum',
+    src: 'https://static.artificialmuseum.com/favicon.png',
+  })
 
 export const style = vars => ({
   '.Header &&': {
